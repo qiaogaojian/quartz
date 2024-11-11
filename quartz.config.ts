893c3937886c104c1e039e1fd4a51285e07596b9
qiaogaojian <qiaogaojian@vip.qq.com>
2024-11-11 17:30:25 +0800

    config: base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "bs2h.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
